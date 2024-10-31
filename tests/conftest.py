066db62d78d65c6551133cf611fd37c0424a6a0d
Leonardo Ferreira <leoventura245@gmail.com>
2024-10-30 21:08:45 -0300

    criando a models

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,13 @@ from fast_zero.app import app
 @pytest.fixture # guarda o resultado da funcao pra passar como parametro em outra funcao
 def client(): # Dont Repeat Yourself
     return TestClient(app)
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
